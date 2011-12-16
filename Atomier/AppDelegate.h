e70281c8f3f3f82122685529b1765f3c3d48fd8b
hideyf <jay@jay-MacBook-Pro.local>
2011-12-16 14:52:17 +0900

    Initial Commit

---
+++ Atomier/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Atomier
+//
+//  Created by Choi Jaeyoung on 12/16/11.
+//  Copyright (c) 2011 Appcid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Atomier/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Atomier
+//
+//  Created by Choi Jaeyoung on 12/16/11.
+//  Copyright (c) 2011 Appcid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
