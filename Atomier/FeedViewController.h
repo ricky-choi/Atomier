fb2f61ae787a8bd22bdcd23f96dfe162ecea9b01
hideyf <jay@jay-MacBook-Pro.local>
2012-01-17 15:12:26 +0900

    ...

--- Atomier/FeedViewController.h
+++ Atomier/FeedViewController.h
@@ -12,25 +12,34 @@
 
 @class Feed;
 
+
 @interface FeedViewController : WebViewController <UIWebViewDelegate, UIScrollViewDelegate, UIActionSheetDelegate, MFMailComposeViewControllerDelegate>
 
 @property (strong, nonatomic) NSArray *feeds;
 @property (strong, nonatomic) Feed *feed;
 
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *previousButtonItem;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *nextButtonItem;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *unreadItem;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *starredItem;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *actionItem;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *previousButtonItem;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *nextButtonItem;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *unreadItem;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *starredItem;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *actionItem;
+
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *toggleListItem;
 
 @property (strong, nonatomic) UIView *topView;
 @property (strong, nonatomic) UIView *bottomView;
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+- (void)replaceWithNewFeed:(Feed *)newFeed direction:(BOOL)up;
 
 - (IBAction)previousFeed:(id)sender;
 - (IBAction)nextFeed:(id)sender;
 - (IBAction)toggleUnread:(id)sender;
 - (IBAction)toggleStarred:(id)sender;
 - (IBAction)shareAction:(id)sender;
+
+- (NSString *)feedTitle;
 
 
 @end
